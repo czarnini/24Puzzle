b7284a38fea858906b1f4af8930e71ade5e60384
czarnini <mboguck2@elka.pw.edu.pl>
2016-05-18 10:17:40 +0200

    Jeszcze kilka zmian, żeby było zgodnie ze sztuką

--- src/_24Puzzle.java
+++ src/_24Puzzle.java
@@ -1,18 +1,33 @@
-import java.awt.EventQueue;
 import java.io.IOException;
 
+import javax.swing.SwingUtilities;
+
 public class _24Puzzle {
-//Event Dispatch thread???
+	GameControl game;
+	AlgorithmControler algContr;
+	GVControler GVContr;
 	
+	public _24Puzzle() throws IOException {
+		 game = new GameControl();
+		 algContr = new AlgorithmControler(game);
+		 GVContr = new GVControler(game);
+		game.AddPuzzles();
+	}
 	
 	public static void main(String[] args) throws InterruptedException, IOException 
 	{
+		SwingUtilities.invokeLater(new Runnable() {
 			
-			GameControl game = new GameControl();
-			AlgorithmControler algContr = new AlgorithmControler(game);
-			GVControler GVContr = new GVControler(game);
-			game.AddPuzzles();
-			//game.randomize();
-			//algContr.solveGame();
+			@Override
+			public void run() {
+				try {
+					@SuppressWarnings("unused")
+					_24Puzzle newGame = new _24Puzzle();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});	
+		
 	}
 }
